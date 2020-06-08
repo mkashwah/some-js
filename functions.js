5379b9614ab40e613b7ce0a9f6791a6bdd9c5e28
M. Kashwah <mkashwah@ryerson.ca>
2020-06-08 08:45:35 -0400

    added couple of functions

--- functions.js
+++ functions.js
@@ -1,1 +1,16 @@
-alert("Connected");
+function showAlert(){
+	alert("Connected");
+}
+
+//function with arguments
+function square(num){
+	let n = num * num;
+	return n;
+}
+
+function factorial(num){
+	if (num > 1){
+		return num * factorial(num - 1);
+	}
+}
+
