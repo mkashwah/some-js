3bd6a6ca53c65eaec0dcfebe28468d9d59e9422c
M. Kashwah <mkashwah@ryerson.ca>
2020-06-09 20:28:19 -0400

    bunch of methods for arrays

--- arrays.js
+++ arrays.js
@@ -1,1 +1,37 @@
-alert("Connected")
+var colors = ["red", "orange", "lll"];
+
+//adding to the end of the array
+//arr.push(item);
+
+colors.push(5);
+
+//removing from the end of the array 
+//arr.pop()
+
+colors.pop()
+
+
+//arr.unshift(item) : add itm to the front of the array
+//arr.shift(): remove item fromn the beginning of the array 
+
+colors.unshift("flooow");
+
+
+//arr.indexOf(item): return the index of the item in the array
+//find if the item exists in teh array
+//returns -1 if the item does not exist
+
+console.log(colors.indexOf("bllllll"));
+
+
+//arr.slice(starting index, finishing index): takes part of the array from beginning to end
+//does not work backwards
+colors.push("bbfdhsf");
+colors.push("dsds");
+colors.push("gf");
+colors.push("jjj");
+
+//////////////////
+//nested or 2-dimensional array
+
+
