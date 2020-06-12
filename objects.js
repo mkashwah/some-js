0d55a8e3a822473a041c28b2910062772665f3d2
M. Kashwah <mkashwah@ryerson.ca>
2020-06-12 01:18:03 -0400

    objects.js

--- objects.js
+++ objects.js
@@ -5,3 +5,17 @@ var dog = {
 
 //arrays vs objects
 //arrays are bound to indices... objects are like dictionaries in python
+
+//nested objects and arrays
+var posts = [
+	{
+		title: "title 1",
+		author: "author 1",
+		comments:["awesome","not awesome"]
+	},
+	{
+		title: "title 2",
+		author: "author 2",
+		comments: ["aaa", "bbb"]
+	}
+]
